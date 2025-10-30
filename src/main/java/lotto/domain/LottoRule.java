7f4e049dc37da494f31d55229dcaee2b908888ee
Seokwoo <seokwoo1999@naver.com>
2025-10-30 13:44:30 +0900

    feat(LottoRule): 로또 규칙을 저장하는 클래스 구현
    - MIN_NUMBER, MAX_NUMBER, SIZE 상수 저장

---
+++ src/main/java/lotto/domain/LottoRule.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class LottoRule {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int SIZE = 6;
+
+    private LottoRule() {}
+}
