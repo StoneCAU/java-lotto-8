968379090006c8e1deb227ecaabc13b146302587
Seokwoo <seokwoo1999@naver.com>
2025-10-30 22:36:57 +0900

    refactor(Money): 생성자 파라미터명 변경
    - purchaseAmount -> amount

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,10 +5,10 @@ public class Money {
     private static final int LOTTO_PRICE = 1000;
     private final int amount;
 
-    public Money(int purchaseAmount) {
-        validatePositive(purchaseAmount);
-        validateDivisibleByLottoPrice(purchaseAmount);
-        this.amount = purchaseAmount;
+    public Money(int amount) {
+        validatePositive(amount);
+        validateDivisibleByLottoPrice(amount);
+        this.amount = amount;
     }
 
     public int calculateLottoCount() {
