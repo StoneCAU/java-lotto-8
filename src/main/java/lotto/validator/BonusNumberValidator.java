dd1e60e930c4ccc1ce0e5e93adcd5327061e6afc
Seokwoo <seokwoo1999@naver.com>
2025-10-31 21:57:19 +0900

    refactor(BonusNumberValidator): 보너스 번호 범위 예외 메세지 처리 방식 변경
    - String.format()을 통해 일관성 유지

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -15,7 +15,7 @@ public class BonusNumberValidator {
 
     private static void validateRange(int bonusNumber) {
         if (bonusNumber < LottoRule.MIN_NUMBER || bonusNumber > LottoRule.MAX_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 " + LottoRule.MIN_NUMBER + "부터 " + LottoRule.MAX_NUMBER + " 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(String.format("[ERROR] 보너스 번호는 %d부터 %d 사이의 숫자여야 합니다.", LottoRule.MIN_NUMBER, LottoRule.MAX_NUMBER));
         }
     }
 
