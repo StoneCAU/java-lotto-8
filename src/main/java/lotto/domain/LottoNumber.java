f1587931098b19947b19553327f2136543094617
Seokwoo <seokwoo1999@naver.com>
2025-11-03 23:47:18 +0900

    refactor(LottoNumber): 에러메세지 상수화

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,9 +2,13 @@ package lotto.domain;
 
 public record LottoNumber(int value) {
 
+    private static final String ERROR_OUT_OF_RANGE = "[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.";
+
     public LottoNumber {
         if (value < LottoConstants.MIN_NUMBER || value > LottoConstants.MAX_NUMBER) {
-            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", LottoConstants.MIN_NUMBER, LottoConstants.MAX_NUMBER));
+            throw new IllegalArgumentException(
+                    String.format(ERROR_OUT_OF_RANGE, LottoConstants.MIN_NUMBER, LottoConstants.MAX_NUMBER)
+            );
         }
     }
 }
